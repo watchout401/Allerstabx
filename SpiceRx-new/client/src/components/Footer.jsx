9a90505c9415e4be323cedc210ae4c77c8d254bc
Abhishek Gond <abhishek24109@iiitd.ac.in>
2025-08-21 23:47:04 +0530

    Delete SpiceRx-new directory

--- SpiceRx-new/client/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './footer.css';
-
-function Footer() {
-  return <footer className="footer">© 2025 Spices Information Website</footer>;
-}
-
-export default Footer;
--- SpiceRx-new/server/routes/spiceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getSpices, getSpiceById } = require('../controllers/spiceController');
-
-const router = express.Router();
-
-router.get('/', getSpices);
-router.get('/:id', getSpiceById);
-
-module.exports = router;
